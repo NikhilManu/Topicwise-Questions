e71684cc8b0710c705000fa75b9fce319d016ba5
NikhilManu <nikhilsmanu@gmail.com>
2021-10-16 15:40:48 +0530

    Cyclic Sorting

---
+++ Sort/CyclicSort.py
@@ -0,0 +1,13 @@
+def CyclicSort(array):
+    idx = 0 
+    while idx < len(array):
+        correctIdx = array[idx] - 1
+        if array[idx] != correctIdx:
+            array[idx], array[correctIdx] = array[correctIdx], array[idx]
+        else:
+            idx += 1
+    return array
+
+# Cyclic Sort is done when array of range [1, n] is given
+# Here on the worse Case 2n - 1 Comaparsions are made 
+# ie.. N swaps and the N - 1 comparisons
