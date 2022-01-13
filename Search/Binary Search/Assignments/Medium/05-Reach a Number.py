5daf0d186a57cbfdf42f6f37b5e62da29229ede5
NikhilManu <nikhilsmanu@gmail.com>
2022-01-13 23:30:21 +0530

    Added Logic and Approach

--- Search/Binary Search/Assignments/Medium/05-Reach a Number.py
+++ Search/Binary Search/Assignments/Medium/05-Reach a Number.py
@@ -1,7 +1,6 @@
 # https://leetcode.com/problems/reach-a-number/
 
-# 
-# Didnt understand the logic of using binary search...Need to revisit
+# Time O(K) | Space O(1)
 def ReachNumber(target):
     """
     Let say our target -> 13
@@ -34,3 +33,39 @@ def ReachNumber(target):
             return k + 1
         else:
             return k + 2
+
+# Approach II - Binary Search the number or steps
+# Time O(log(Target)) | Space O(1)
+def reachNumber(target):
+    steps = Bsearch(abs(target))
+    diff = steps * (steps + 1) // 2 - abs(target)
+    
+    """
+        Adjust steps:
+        - Determine the difference (diff) between the total
+          distance traveled, min_steps * (min_steps + 1) // 2,
+          and target.
+        - If diff is 0, min_steps does not change.
+        - If diff is even, it is possible to flip a step from
+          positive to negative to get target. Again, min_steps
+          does not change.
+        - If diff is odd, we need to add one or two steps to
+          create an even difference, at which point we can flip
+          one or more steps to get target.
+    """
+    if diff % 2:
+        return steps + 1 + steps % 2 # same as above case, Just wrote it in one line
+    return steps
+
+def Bsearch(target):
+    start, end = 0, abs(target)
+    while start < end:
+        minSteps = start + (end - start) // 2
+        
+        canReach = minSteps * (minSteps + 1) // 2 >= target
+        if canReach:
+            end = minSteps
+        else:
+            start = minSteps + 1
+            
+    return end
